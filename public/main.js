7c49b61328a4d930e3b333802fb2abee75ba977f
Eric Pedreschi <ericpedreschi@gmail.com>
2022-06-04 21:21:59 -0500

    mvp de descarga de colecciones

--- public/main.js
+++ public/main.js
@@ -1,6 +1,14 @@
-import { guardarDoc } from './modulos/downDoc.js'
+// import { guardarDoc } from './modulos/downDoc.js'
 import { guardarColl } from './modulos/downColl.js'
 
 
 // guardarDoc()
-guardarColl("CLIENTES")
+
+
+
+
+
+// descomenta esta linea para descargar una coleccion entera
+// CUIDADO: PUEDE TRAER MUCHOS DOCUMENTOS AL EJECUTARSE
+// guardarColl("clientes");
+// solo funciona con las colecciones de la raiz
--- public/modulos/downColl.js
+++ public/modulos/downColl.js
@@ -6,14 +6,13 @@ import { getFirestore, collection, getDocs } from "https://www.gstatic.com/fireb
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAdpsQab-Ys38Gy3BqCJOh4xMRJV6oofQI",
-  authDomain: "strongdog-2.firebaseapp.com",
-  databaseURL: "https://strongdog-2-default-rtdb.firebaseio.com",
-  projectId: "strongdog-2",
-  storageBucket: "strongdog-2.appspot.com",
-  messagingSenderId: "335477571403",
-  appId: "1:335477571403:web:2fe73fb45b7cf80435b2ce"
-};
+    apiKey: "AIzaSyDswkNyh3K8gSHprFxMPh8yYIUHKKoVM48",
+    authDomain: "strongdog-project.firebaseapp.com",
+    projectId: "strongdog-project",
+    storageBucket: "strongdog-project.appspot.com",
+    messagingSenderId: "636171274219",
+    appId: "1:636171274219:web:6b70f0aa188c5cb51a3bae"
+  };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
@@ -33,7 +32,7 @@ export async function guardarColl(coleccion) {
     var dlAnchorElem = document.getElementById('downloadAnchorElem');
     dlAnchorElem.setAttribute("href",     dataStr     );
     dlAnchorElem.setAttribute("download", `${querySnapshot.query.id}.json`);
-    dlAnchorElem.click();
+    // dlAnchorElem.click();
 
 }
 
